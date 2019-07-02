858581c57dfbabafa924aac123509f95c379c465
huruipeng <huruipeng@jd.com>
2019-07-02 15:23:01 +0800

    抽象策略类接口

---
+++ src/main/java/com/my/designpattern/strategy/service/Buyer.java
@@ -0,0 +1,13 @@
+package com.my.designpattern.strategy.service;
+
+/**
+ * @ClassName Buyer
+ * @Description TODO
+ * @Author 61
+ * @Date 2019/7/2 15:22
+ * @Version 1.0
+ * @Copyright: Copyright (c) 2018
+ **/
+
+public class Buyer {
+}
