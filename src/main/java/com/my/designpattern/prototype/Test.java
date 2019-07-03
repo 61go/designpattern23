bf6436ac87dc2506d57ad2c403ed544d7ce325b8
huruipeng <huruipeng@jd.com>
2019-07-03 09:50:09 +0800

    添加原型模式

--- src/main/java/com/my/designpattern/prototype/Test.java
+++ src/main/java/com/my/designpattern/prototype/Test.java
@@ -2,9 +2,10 @@ package com.my.designpattern.prototype;
 
 public class Test {
     public static void main(String[] args) throws CloneNotSupportedException {
-        JoyModel jm = new JoyModel("中华人民共和国", 53);
-        JoyModel joy2 = (JoyModel) jm.clone();
+        JoyModel joy1 = new JoyModel("中华人民共和国", 53);
+        JoyModel joy2 = (JoyModel) joy1.clone();
         joy2.test();
+        System.out.println(joy1 == joy2);
 
     }
 }
