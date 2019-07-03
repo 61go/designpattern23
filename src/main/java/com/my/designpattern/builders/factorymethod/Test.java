837ed63e60d53b42ace7e2ca58784c82489b2231
huruipeng <huruipeng@jd.com>
2019-07-03 11:46:02 +0800

    添加工厂方法模式

--- src/main/java/com/my/designpattern/builders/factorymethod/Test.java
+++ src/main/java/com/my/designpattern/builders/factorymethod/Test.java
@@ -8,7 +8,7 @@ import com.my.designpattern.builders.factorymethod.product.Animal;
  * @Author huruipeng
  * @Description 注意工厂方法模式，是将工厂抽象同时产品也被抽象了！通过抽象层发生关联
  * @Date 2019/7/3 11:44
- * @Param
+ * @Param   @See http://c.biancheng.net/view/1348.html
  * @creator huruipeng
  * @return
  **/
