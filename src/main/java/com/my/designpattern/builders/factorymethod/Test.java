3faba9a0cf41cc7196880b6347e25730682f8a04
huruipeng <huruipeng@jd.com>
2019-07-03 12:02:36 +0800

    添加抽象工厂模式

--- src/main/java/com/my/designpattern/builders/factorymethod/Test.java
+++ src/main/java/com/my/designpattern/builders/factorymethod/Test.java
@@ -6,7 +6,7 @@ import com.my.designpattern.builders.factorymethod.product.Animal;
 
 /**
  * @Author huruipeng
- * @Description 注意工厂方法模式，是将工厂抽象同时产品也被抽象了！通过抽象层发生关联
+ * @Description 注意工厂方法模式，是将工厂抽象同时产品也被抽象了！通过抽象层发生关联，但是只是单一的工厂生产单一的产品
  * @Date 2019/7/3 11:44
  * @Param   @See http://c.biancheng.net/view/1348.html
  * @creator huruipeng
