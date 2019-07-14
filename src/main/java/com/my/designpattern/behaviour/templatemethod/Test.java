ed6083b3bcfe3b6ad174f1cca01ef7691ec43e87
hruipeng61@gamil.com <hruipeng61@gamil.com>
2019-07-15 01:48:19 +0800

    模板模式

--- src/main/java/com/my/designpattern/behaviour/templatemethod/Test.java
+++ src/main/java/com/my/designpattern/behaviour/templatemethod/Test.java
@@ -2,7 +2,8 @@ package com.my.designpattern.behaviour.templatemethod;
 
 /**
  * @program: Test
- * @description: 模板方法很简单其实。。。。就是抽象类把一系列动作（有顺序的）的步骤给抽象出来，然后继承类实现步骤即可。
+ * @description: 模板方法很简单其实。。。。就是抽象类把一系列动作（有顺序的）的步骤给抽象出来，然后由继承类去实现
+ * 很经典的一种模式
  * @author: Caffeine61
  * @create: 2019-07-15 01:43
  **/
