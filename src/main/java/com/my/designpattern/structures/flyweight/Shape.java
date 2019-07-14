fae5c6b2f599fca17d767d2f764a3035ef55061b
hruipeng61@gamil.com <hruipeng61@gamil.com>
2019-07-15 01:29:31 +0800

    享元模式

---
+++ src/main/java/com/my/designpattern/structures/flyweight/Shape.java
@@ -0,0 +1,5 @@
+package com.my.designpattern.structures.flyweight;
+
+public interface Shape {
+   void draw();
+}
---
+++ src/main/java/com/my/designpattern/structures/flyweight/Test.java
@@ -0,0 +1,32 @@
+package com.my.designpattern.structures.flyweight;
+
+/**
+ * @program: Test
+ * @description: 享元模式很有意思呢，可以节省好多内存呢Q，而且避免创建速度快
+ * @author: Caffeine61
+ * @create: 2019-07-15 01:20
+ **/
+
+
+public class Test {
+    public static final String[] COLORS = {"Red", "Green", "Blue", "White", "Black"};
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            final Circle circle =(Circle) ShapeFactory.getCircle(getRandomColor());
+            circle.setX(getRandomX());
+            circle.setY(getRandomY());
+            circle.setRadius(99);
+            circle.draw();
+        }
+    }
+
+    private static String getRandomColor() {
+        return COLORS[(int)(Math.random()*COLORS.length)];
+    }
+    private static int getRandomX() {
+        return (int)(Math.random()*100 );
+    }
+    private static int getRandomY() {
+        return (int)(Math.random()*100);
+    }
+}
